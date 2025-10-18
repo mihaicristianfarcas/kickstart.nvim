c3cb9701c0a6d8158f7b3145fbd9ad1e90073784
Mihai-Cristian Farcaș <146859576+mihaicristianfarcas@users.noreply.github.com>
2025-10-18 14:58:25 +0300

    new: harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,40 @@
+-- You can add your own plugins here or in other files in this directory!
+-- I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      -- Load harpoon
+      local harpoon = require 'harpoon'
+
+      -- REQUIRED setup call
+      harpoon:setup()
+
+      -- Basic keymaps
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end, { desc = 'Harpoon: Add file' })
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon: Toggle menu' })
+
+      -- Optional quick jump bindings
+      vim.keymap.set('n', '<C-h>', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon: File 1' })
+      vim.keymap.set('n', '<C-t>', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon: File 2' })
+      vim.keymap.set('n', '<C-n>', function()
+        harpoon:list():select(3)
+      end, { desc = 'Harpoon: File 3' })
+      vim.keymap.set('n', '<C-s>', function()
+        harpoon:list():select(4)
+      end, { desc = 'Harpoon: File 4' })
+    end,
+  },
+}
