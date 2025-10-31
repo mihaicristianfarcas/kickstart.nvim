15511468b241af1dbe7a18ed0c9223bfdddb30c9
Mihai-Cristian Farcaș <146859576+mihaicristianfarcas@users.noreply.github.com>
2025-10-31 15:31:41 +0200

    fix: harpoon quickjump consistent keybindings

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -23,16 +23,16 @@ return {
       end, { desc = 'Harpoon: Toggle menu' })
 
       -- Optional quick jump bindings
-      vim.keymap.set('n', '<C-h>', function()
+      vim.keymap.set('n', '1<C-e>', function()
         harpoon:list():select(1)
       end, { desc = 'Harpoon: File 1' })
-      vim.keymap.set('n', '<C-t>', function()
+      vim.keymap.set('n', '2<C-e>', function()
         harpoon:list():select(2)
       end, { desc = 'Harpoon: File 2' })
-      vim.keymap.set('n', '<C-n>', function()
+      vim.keymap.set('n', '3<C-e>', function()
         harpoon:list():select(3)
       end, { desc = 'Harpoon: File 3' })
-      vim.keymap.set('n', '<C-s>', function()
+      vim.keymap.set('n', '4<C-e>', function()
         harpoon:list():select(4)
       end, { desc = 'Harpoon: File 4' })
     end,
