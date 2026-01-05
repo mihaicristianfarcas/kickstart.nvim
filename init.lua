a3fb0f36cf6b6d66f4d9f21e428399679fa41d5f
Mihai-Cristian Farcaș <146859576+mihaicristianfarcas@users.noreply.github.com>
2026-01-05 21:11:41 +0200

    fix: nvim-treesitter breaking changes, removed codewindow bc of treesitter and default theme

--- init.lua
+++ init.lua
@@ -920,29 +920,29 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        transparent = true,
-        styles = {
-          comments = { italic = true }, -- Enable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
-
+  -- -- { -- You can easily change to a different colorscheme.
+  -- --   -- Change the name of the colorscheme plugin below, and then
+  -- --   -- change the command in the config to whatever the name of that colorscheme is.
+  -- --   --
+  -- --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- --   'folke/tokyonight.nvim',
+  -- --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- --   config = function()
+  -- --     ---@diagnostic disable-next-line: missing-fields
+  -- --     require('tokyonight').setup {
+  -- --       transparent = true,
+  -- --       styles = {
+  -- --         comments = { italic = true }, -- Enable italics in comments
+  -- --       },
+  -- --     }
+  -- --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --   end,
+  -- },
+  --
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -986,7 +986,6 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
